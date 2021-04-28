0a49b94753dcd1f08c6061b80185c80ee5a09d61
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-04-27 22:15:05 -0300

    add interactor (AddBook)

---
+++ spec/bookshelf/interactors/add_book_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe AddBook do
+  let(:interactor) { AddBook.new }
+  let(:attributes) { Hash[author: 'James Baldwin', title: 'The fire next time'] }
+
+  context 'good input' do
+    let(:result) { interactor.call(attributes) }
+
+    it 'succeeds' do
+      expect(result.successful?).to be_truthy
+    end
+
+    it 'creates a book with correct title and author' do
+      expect(result.book.title).to eq('The fire next time')
+      expect(result.book.author).to eq('James Baldwin')
+    end
+
+    context 'persistence' do
+      let(:repository) { instance_double('BookRepository') }
+
+      it 'persists the book' do
+        expect(repository).to receive(:create)
+        AddBook.new(repository: repository).call(attributes)
+      end
+    end
+  end
+end
